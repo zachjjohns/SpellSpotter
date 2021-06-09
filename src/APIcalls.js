0e95de0b7f53f1e8f60b16feeee94a7224e3e98e
Zach Johnson <zachjjohns@gmail.com>
2021-06-09 11:34:03 -0500

    Create APIcalls with first fetch/errorhandler function

---
+++ src/APIcalls.js
@@ -0,0 +1,13 @@
+export const getSpells = async () => {
+  const response = await fetch('https://www.dnd5eapi.co/api/spells');
+  const data = await errorHandler(response);
+  return data
+}
+
+export const errorHandler = (response) => {
+  if (!response.ok) {
+    throw new Error(response.message)
+  } else {
+    return response.json()
+  }
+}
