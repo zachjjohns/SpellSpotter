19a5ea3703e013b9e73b097a17d34042f823af2a
Zach Johnson <zachjjohns@gmail.com>
2021-06-10 15:07:14 -0500

    Add h1 to Header and import into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Header from '../Header/Header';
 import {getSpells, getSingleSpell} from '../../APIcalls';
 import React, { Component } from 'react';
 
@@ -12,9 +13,7 @@ export default class App extends Component {
   
   render() {
     return (
-      <div>
-        <h1>Spellz!</h1>
-      </div>
+      <Header />
     )
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-      
+      <h1 className="app-name">Spell Spotter</h1>
     </header>
   )
 }
