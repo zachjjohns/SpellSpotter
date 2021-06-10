a4b898b708f1c5a2c7f5dbac0298cb2e572b7281
Zach Johnson <zachjjohns@gmail.com>
2021-06-10 17:04:42 -0500

    Add levels 1 thru 9 in SpellSlots state

--- src/components/SpellSlots/SpellSlots.js
+++ src/components/SpellSlots/SpellSlots.js
@@ -3,6 +3,21 @@ import './SpellSlots.css';
 import React, { Component } from 'react'
 
 export default class SpellSlots extends Component {
+  constructor() {
+    super()
+    this.state = {
+      level1: 0,
+      level2: 0,
+      level3: 0,
+      level4: 0,
+      level5: 0,
+      level6: 0,
+      level7: 0,
+      level8: 0,
+      level9: 0,
+    }
+  }
+  
   render() {
     return (
       <div>
