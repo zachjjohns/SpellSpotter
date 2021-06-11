16217960f651b895cf4ad8a87bd322c7711bcb4f
Zach Johnson <zachjjohns@gmail.com>
2021-06-11 10:47:13 -0500

    Create map JSX to render SpellSelect options

--- src/components/SpellSelect/SpellSelect.js
+++ src/components/SpellSelect/SpellSelect.js
@@ -4,6 +4,10 @@ import React from 'react'
 
 export default function SpellSelect({ spells }) {
   return (
-    <select className="spells-dropdown">{console.log(spells)}</select>
+    <select className="spells-dropdown">
+      {spells.map((spell) => (
+        <option value={spell.index}>{spell.name}</option>
+      ))}
+    </select>
   )
 }
