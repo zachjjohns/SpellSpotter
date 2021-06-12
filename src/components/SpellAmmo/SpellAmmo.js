658dff652d86fbf2cc755b7a25a2eae2488d4e13
Zach Johnson <zachjjohns@gmail.com>
2021-06-12 13:42:54 -0500

    UAdd className + CSS for ammo-p

--- src/components/SpellAmmo/SpellAmmo.js
+++ src/components/SpellAmmo/SpellAmmo.js
@@ -27,39 +27,39 @@ export default class SpellAmmo extends Component {
         <div className="ammo-wrapper">
           <div className="level-container">
             <p>1st</p>
-            <p>{this.state.level1}</p>
+            <p className="ammo-p">{this.state.level1}</p>
           </div>
           <div className="level-container">
             <p>2nd</p>
-            <p>{this.state.level2}</p>
+            <p className="ammo-p">{this.state.level2}</p>
           </div>
           <div className="level-container">
             <p>3rd</p>
-            <p>{this.state.level3}</p>
+            <p className="ammo-p">{this.state.level3}</p>
           </div>
           <div className="level-container">
             <p>4th</p>
-            <p>{this.state.level4}</p>
+            <p className="ammo-p">{this.state.level4}</p>
           </div>
           <div className="level-container">
             <p>5th</p>
-            <p>{this.state.level5}</p>
+            <p className="ammo-p">{this.state.level5}</p>
           </div>
           <div className="level-container">
             <p>6th</p>
-            <p>{this.state.level6}</p>
+            <p className="ammo-p">{this.state.level6}</p>
           </div>
           <div className="level-container">
             <p>7th</p>
-            <p>{this.state.level7}</p>
+            <p className="ammo-p">{this.state.level7}</p>
           </div>
           <div className="level-container">
             <p>8th</p>
-            <p>{this.state.level8}</p>
+            <p className="ammo-p">{this.state.level8}</p>
           </div>
           <div className="level-container">
             <p>9th</p>
-            <p>{this.state.level9}</p>
+            <p className="ammo-p">{this.state.level9}</p>
           </div>
         </div>
       </section>
