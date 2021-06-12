12f3b4ab9fe47cac30f5d37b30c754037366272a
Zach Johnson <zachjjohns@gmail.com>
2021-06-12 13:28:24 -0500

    Add starter CSS for SpellAmmo

--- src/components/SpellAmmo/SpellAmmo.js
+++ src/components/SpellAmmo/SpellAmmo.js
@@ -23,11 +23,43 @@ export default class SpellAmmo extends Component {
   render() {
     return (
       <section className="ammo-section">
-        <h2>Today's Spell Ammo</h2>
+        <h2 className="todays-ammo">Today's Spell Ammo</h2>
         <div className="ammo-wrapper">
           <div className="level-container">
             <p>1st Level</p>
             <p>{this.state.level1}</p>
+          </div>
+          <div className="level-container">
+            <p>2nd Level</p>
+            <p>{this.state.level2}</p>
+          </div>
+          <div className="level-container">
+            <p>3rd Level</p>
+            <p>{this.state.level3}</p>
+          </div>
+          <div className="level-container">
+            <p>4th Level</p>
+            <p>{this.state.level4}</p>
+          </div>
+          <div className="level-container">
+            <p>5th Level</p>
+            <p>{this.state.level5}</p>
+          </div>
+          <div className="level-container">
+            <p>6th Level</p>
+            <p>{this.state.level6}</p>
+          </div>
+          <div className="level-container">
+            <p>7th Level</p>
+            <p>{this.state.level7}</p>
+          </div>
+          <div className="level-container">
+            <p>8th Level</p>
+            <p>{this.state.level8}</p>
+          </div>
+          <div className="level-container">
+            <p>9th Level</p>
+            <p>{this.state.level9}</p>
           </div>
         </div>
       </section>
