c14f37ec142815a28f4f6745396645391f9a9bcf
Zach Johnson <zachjjohns@gmail.com>
2021-06-12 12:50:20 -0500

    Add level1-level9 to state

--- src/components/SpellAmmo/SpellAmmo.js
+++ src/components/SpellAmmo/SpellAmmo.js
@@ -3,6 +3,22 @@ import './SpellAmmo.css';
 import React, { Component } from 'react'
 
 export default class SpellAmmo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      level1: 0,
+      level2: 0,
+      level3: 0,
+      level4: 0,
+      level5: 0,
+      level6: 0,
+      level7: 0,
+      level8: 0,
+      level9: 0,
+      error: ""
+    }
+  }
+  
   render() {
     return (
       <div>
