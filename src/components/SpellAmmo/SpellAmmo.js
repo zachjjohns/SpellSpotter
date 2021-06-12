525908b85d6ebe7ff3f93eae125a54a8de1c9c3f
Zach Johnson <zachjjohns@gmail.com>
2021-06-12 13:30:05 -0500

    Remove word 'Level' from each slot, add to heading instead

--- src/components/SpellAmmo/SpellAmmo.js
+++ src/components/SpellAmmo/SpellAmmo.js
@@ -23,42 +23,42 @@ export default class SpellAmmo extends Component {
   render() {
     return (
       <section className="ammo-section">
-        <h2 className="todays-ammo">Today's Spell Ammo</h2>
+        <h2 className="todays-ammo">Today's Spell Ammo by Level</h2>
         <div className="ammo-wrapper">
           <div className="level-container">
-            <p>1st Level</p>
+            <p>1st</p>
             <p>{this.state.level1}</p>
           </div>
           <div className="level-container">
-            <p>2nd Level</p>
+            <p>2nd</p>
             <p>{this.state.level2}</p>
           </div>
           <div className="level-container">
-            <p>3rd Level</p>
+            <p>3rd</p>
             <p>{this.state.level3}</p>
           </div>
           <div className="level-container">
-            <p>4th Level</p>
+            <p>4th</p>
             <p>{this.state.level4}</p>
           </div>
           <div className="level-container">
-            <p>5th Level</p>
+            <p>5th</p>
             <p>{this.state.level5}</p>
           </div>
           <div className="level-container">
-            <p>6th Level</p>
+            <p>6th</p>
             <p>{this.state.level6}</p>
           </div>
           <div className="level-container">
-            <p>7th Level</p>
+            <p>7th</p>
             <p>{this.state.level7}</p>
           </div>
           <div className="level-container">
-            <p>8th Level</p>
+            <p>8th</p>
             <p>{this.state.level8}</p>
           </div>
           <div className="level-container">
-            <p>9th Level</p>
+            <p>9th</p>
             <p>{this.state.level9}</p>
           </div>
         </div>
