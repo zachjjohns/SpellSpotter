f79671d5b29ec8723985eea59af7f3ee5f40a489
Zach Johnson <zachjjohns@gmail.com>
2021-06-13 12:23:57 -0500

    Add starter JSX for Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,10 +2,14 @@ import './Card.css';
 
 import React from 'react'
 
-export default function Card() {
+export default function Card({ id, name, level, desc, range, attack_type }) {
   return (
-    <div className="spell-card">
-      <p></p>
+    <div className="spell-card" id={id}>
+      <p className="spell-name">{name}</p>
+      <p className="spell-level">{level}</p>
+      <p className="spell-desc">{desc}</p>
+      <p className="spell-range">{range}</p>
+      <p className="spell-attack-type">{attack_type}</p>
     </div>
   )
 }
