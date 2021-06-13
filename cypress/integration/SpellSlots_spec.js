b713a8ae7d2b5e8fa3e18c770cdff0a1cf83827f
Zach Johnson <zachjjohns@gmail.com>
2021-06-13 15:58:45 -0500

    Add test for checking labels/inputs

--- cypress/integration/SpellSlots_spec.js
+++ cypress/integration/SpellSlots_spec.js
@@ -31,7 +31,21 @@ describe('SpellSlots', () => {
   ]})
       .visit('http://localhost:3000')
   })
+
   it('Should display an intro message', () => {
     cy.get('.slots-intro').should('have.text', 'First, please enter your maximum Spell Slots per Spell Level')
+  })
+  
+  it('Should display labels + inputs for each spell level', () => {
+    cy.get('label').should('have.text', '1st2nd3rd4th5th6th7th8th9th')
+    cy.get('form input[name="level1"]')
+    cy.get('form input[name="level2"]')
+    cy.get('form input[name="level3"]')
+    cy.get('form input[name="level4"]')
+    cy.get('form input[name="level5"]')
+    cy.get('form input[name="level6"]')
+    cy.get('form input[name="level7"]')
+    cy.get('form input[name="level8"]')
+    cy.get('form input[name="level9"]')
   })
 })
