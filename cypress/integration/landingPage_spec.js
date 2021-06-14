8995f5094ab1dc36c557f3cfb04f1f7bc05bac2c
Zach Johnson <zachjjohns@gmail.com>
2021-06-14 15:02:14 -0500

    Add tests for Bad URL handling

--- cypress/integration/landingPage_spec.js
+++ cypress/integration/landingPage_spec.js
@@ -80,3 +80,16 @@ describe('Spells Error Handling', () => {
       .should('have.text', 'Could not retrieve spells! The Head Wizard probably found the ale again.')
   })
 })
+
+describe('Bad URL Handling', () => {
+  it('Should display an error message upon a bad URL', () => {
+    cy.visit('http://localhost:3000/badurl')
+      .get('.not-found').should('have.text', 'URL Not Found - Click button to return')
+  })
+
+  it('Should have a button that returns to main page when clicked', () => {
+    cy.visit('http://localhost:3000/badurl')
+      .get('.return-home').should('have.text', 'Return Home').click()
+      .url().should('eq', 'http://localhost:3000/')
+  })
+})
